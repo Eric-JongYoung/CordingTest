74b8018d4faffa27f8417f224f8fe3fd97bafeb1
Eric-JongYoung <fgh1937@yahoo.com>
2023-01-03 16:39:12 +0900

    step01 - ch05 특정 문자 뒤집기

---
+++ src/step01/ch05_특정문자뒤집기/Main.java
@@ -0,0 +1,37 @@
+package step01.ch05_특정문자뒤집기;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+
+        String str = kb.next();
+
+        System.out.println(T.solution(str));
+    }
+
+    public String solution(String str){
+        String answer;
+        char[] s = str.toCharArray();
+        int lt = 0;
+        int rt = str.length() - 1;
+        while (lt<rt){
+            if(!Character.isAlphabetic(s[lt])){
+                lt++;
+            }else if(!Character.isAlphabetic(s[rt])){
+                rt--;
+            }else {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+        }
+        answer = String.valueOf(s);
+
+        return answer;
+    }
+}
