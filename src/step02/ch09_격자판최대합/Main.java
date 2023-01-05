5fb9aeb8f383c8737cb41da4dd13c787c12a45d9
Eric-JongYoung <fgh1937@yahoo.com>
2023-01-06 00:02:45 +0900

    step02 - ch09 격자판 최대합

---
+++ src/step02/ch09_격자판최대합/Main.java
@@ -0,0 +1,42 @@
+package step02.ch09_격자판최대합;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = kb.nextInt();
+            }
+        }
+        System.out.println(T.solution(n, arr));
+    }
+
+    public int solution(int n, int[][] arr) {
+        int answer = Integer.MIN_VALUE;
+        // sum1 : 행의 합 sum2 : 열의합
+        int sum1, sum2;
+        for (int i = 0; i < n; i++) {
+            sum1 = sum2 = 0;
+            for (int j = 0; j < n; j++) {
+                sum1 += arr[i][j];
+                sum2 += arr[j][i];
+            }
+            answer = Math.max(answer, sum1);
+            answer = Math.max(answer, sum2);
+        }
+        sum1 = sum2 = 0;
+        for (int i = 0; i < n; i++) {
+            sum1 += arr[i][i];
+            sum2 += arr[i][n - i - 1];
+        }
+        answer = Math.max(answer, sum1);
+        answer = Math.max(answer, sum2);
+
+        return answer;
+    }
+}
