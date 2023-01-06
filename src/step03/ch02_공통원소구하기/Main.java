6c86338f9c0dde0ecbdf0da820d6cf26447fa730
Eric-JongYoung <fgh1937@yahoo.com>
2023-01-06 23:59:00 +0900

    step03 - ch02 - 공통원소 구하기

--- src/step03/ch02_공통원소구하기/Main.java
+++ src/step03/ch02_공통원소구하기/Main.java
@@ -1,6 +1,7 @@
-package step03.ch01_두배열합치기;
+package step03.ch02_공통원소구하기;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,6 +9,7 @@ public class Main {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
+
         int[] a = new int[n];
         for (int i = 0; i < n; i++) {
             a[i] = kb.nextInt();
@@ -20,20 +22,25 @@ public class Main {
         for (int x : T.solution(n, m, a, b)) {
             System.out.print(x + " ");
         }
-
     }
-
     public ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
         ArrayList<Integer> answer = new ArrayList<>();
+        Arrays.sort(a);
+        Arrays.sort(b);
         int p1 = 0, p2 = 0;
         while (p1 < n && p2 < m) {
-            if (a[p1] < b[p2]) answer.add(a[p1++]); // 값을 넣고 p1 증가
-            else answer.add(b[p2++]);
+            if (a[p1] == b[p2]) {
+                answer.add(a[p1++]);
+                p2++;
+            } else if (a[p1] < b[p2]) p1++;
+            else p2++;
         }
-        // 둘 중 하나가 비교가 끝나면 무제한으로 계속 넣도록 설정
-        while (p1 < n) answer.add(a[p1++]);
-        while (p2 < m) answer.add(b[p2++]);
         return answer;
     }
-
 }
+/**
+ * 값을 우선 오름 차순으로 정렬하고
+ * p1, p2 를 임의의 값으로 정해서
+ * 하나씩 증가시키면서 같으면 새로운 배열에 추가해줌과 동시에, p1와 p2를 동시에 증가시킨다.
+ * 만약 같은 값이 안나오면, 반대쪽을 증가시킨다.
+ */
