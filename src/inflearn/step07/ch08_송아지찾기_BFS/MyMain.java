663b9068ecaf5f4235e4ad06160b0e62bdecaa97
Eric-JongYoung <fgh1937@yahoo.com>
2023-01-08 23:55:03 +0900

    step07 - ch08 송아지 찾기 - BFS
    - BFS 를 이용해서 최단거리 찾기

---
+++ src/inflearn/step07/ch08_송아지찾기_BFS/MyMain.java
@@ -0,0 +1,46 @@
+package inflearn.step07.ch08_송아지찾기_BFS;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class MyMain {
+    int answer = 0;
+    int[] dis = {1, 5, -1};
+    int[] ch;
+    Queue<Integer> Q = new LinkedList<>();
+
+    public int BFS(int s, int e) {
+        ch =new int[10001];
+        ch[s] = 1;
+        int L = 0;
+        Q.offer(s);
+        while (!Q.isEmpty()) {
+            int len = Q.size();
+            for (int i = 0; i < len; i++) {
+                int x = Q.poll();
+
+                for (int j = 0; j < 3; j++) {
+                    int nx = x + dis[j];
+                    if(nx == e) return L+1;
+                    if (nx >= 1 && nx <= 10000 && ch[nx] == 0) {
+                        Q.offer(nx);
+                        ch[nx] = 1;
+                    }
+                }
+            }
+            L++;
+        }
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        MyMain T = new MyMain();
+        Scanner kb = new Scanner(System.in);
+        int s = kb.nextInt();
+        int e = kb.nextInt();
+
+        System.out.println(T.BFS(s, e));
+    }
+}
