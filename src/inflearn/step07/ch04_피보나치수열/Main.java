2adc1b0926357d985f6a23e68301eace617a64f0
Eric-JongYoung <fgh1937@yahoo.com>
2023-01-08 18:05:22 +0900

    step07 - ch04 피보나치 수열
    - DFS
    - stack 으로 구성되어서 진행된다.
    - for 문으로 하는게 더 성능은 좋다. 왜냐면 재귀는 스텍 프레임을 이용하기 때문에
    - 재귀로 할거면 MainBestWay.java 이렇게 기존 값을 array에 저장하고 바로 쓸 수 있도록 하면 메모리 낭비를 줄일 수 있다.

---
+++ src/inflearn/step07/ch04_피보나치수열/Main.java
@@ -0,0 +1,23 @@
+package inflearn.step07.ch04_피보나치수열;
+
+public class Main {
+    public static void main(String[] args) {
+        Main T = new Main();
+        int n = 5;
+        for(int i = 1; i <= n ; i ++) {
+            System.out.print(T.DFS(i)+ " ");
+        }
+    }
+
+    public int DFS(int n) {
+        if(n==1) return 1;
+        else if(n==2) return 1;
+        else return DFS(n-2) + DFS(n-1);
+    }
+}
+
+/**
+ * 재귀 함수를 통해서 이미 구해진걸 반복적으로 구하면 -> 메모리 낭비 및 느려짐
+ * 이를 해결하기 위해서,
+ *
+ * */
