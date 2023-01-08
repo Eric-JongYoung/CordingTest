60ea642914e2c1e4a8a450adaa4f6660caf5d7f6
Eric-JongYoung <fgh1937@yahoo.com>
2023-01-08 17:41:58 +0900

    step07 - ch03 팩토리얼
    - DFS
    - stack 으로 구성되어서 진행된다.

---
+++ src/inflearn/step07/ch03_팩토리얼/Main.java
@@ -0,0 +1,13 @@
+package inflearn.step07.ch03_팩토리얼;
+
+public class Main {
+    public static void main(String[] args) {
+        Main T = new Main();
+        System.out.println(T.DFS(5));
+    }
+
+    public int DFS(int n) {
+        if(n==0) return 1;
+        else return n * DFS(n - 1);
+    }
+}
