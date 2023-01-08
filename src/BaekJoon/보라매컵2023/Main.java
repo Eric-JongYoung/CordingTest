6bebbbdea13fcf4b7dfa689db240a485aaa8fa11
Eric-JongYoung <fgh1937@yahoo.com>
2023-01-08 19:06:04 +0900

    BaekJoon - 2023 보라매컵 예선 - A문제

---
+++ src/BaekJoon/보라매컵2023/Main.java
@@ -0,0 +1,30 @@
+package BaekJoon.보라매컵2023;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = kb.nextInt();
+        }
+        System.out.println(T.solution(n, arr));
+    }
+
+    public int solution(int n, int[] arr) {
+        int answer = 0;
+        for (int i = 0; i < 3; i++) {
+            if ((n - arr[i]) >= 0) {
+                answer += arr[i];
+            }else {
+                answer += n;
+            }
+        }
+
+        return answer;
+    }
+}
