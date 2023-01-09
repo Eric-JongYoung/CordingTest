cd1f5240502e64cb4b18bc7e7297671d7c152ca7
Eric-JongYoung <fgh1937@yahoo.com>
2023-01-09 12:24:18 +0900

    step07 - ch12, 13, 14 경로탐색
    - ch11 은 코드가 없음
    - ch12 : 경로 탐색 DFS
    - ch13 : 경로 탐색 ArrayList
    - ch14 : 경로 탐색 BFS

---
+++ src/inflearn/step07/ch12_경로탐색_DFS/Main.java
@@ -0,0 +1,40 @@
+package inflearn.step07.ch12_경로탐색_DFS;
+
+import java.util.Scanner;
+
+public class Main {
+    static int n, m, answer = 0;
+    static int[][] graph;
+    static int[] ch;
+
+    public void DFS(int v) {
+        if(v==n) answer++;
+        else { //뻣아 나감
+            for (int i = 1; i <= n; i++) {
+                if (graph[v][i] == 1 && ch[i] == 0) {
+                    ch[i]=1; //방문했다고 체크
+                    DFS(i);
+                    ch[i]=0; // 백할때 이전에 방문했던 지점 체크 해제
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        m = kb.nextInt();
+        graph = new int[n + 1][n + 1]; // 정점 번호를 의미
+        ch = new int[n + 1];
+        for (int i = 0; i < m; i++) {
+            int a = kb.nextInt();
+            int b = kb.nextInt();
+            graph[a][b] = 1;
+        }
+        ch[1] = 1; // 출발점 체크
+        T.DFS(1);
+        System.out.println(answer);
+    }
+}
