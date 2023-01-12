b2ce896ffc3ac393bede563d677eafa5c315395a
Eric-JongYoung <fgh1937@yahoo.com>
2023-01-12 13:10:17 +0900

    step06 - ch02,03 버블정렬, 삽입정렬
    - 버블정렬
    - 삽입정렬

--- src/inflearn/step06_Sort_and_Searching/ch01_선택정렬/Main.java
+++ src/inflearn/step06_Sort_and_Searching/ch01_선택정렬/Main.java
@@ -18,7 +18,7 @@ public class Main {
 
     public int[] solution(int n, int[] arr) {
         for (int i = 0; i < n-1 ; i++) { // 마지막 자리 까지 갈 필요 없으니까 한번이라도 덜 돌게
-            int idx = i; // idx 는 배열에서 가장 작은 값을 가진 배열의 index 를 저
+            int idx = i; // idx 는 배열에서 가장 작은 값을 가진 배열의 index 를 저장
             for (int j = i + 1; j < n; j++) {
                 if(arr[j]<arr[idx]) idx = j;
             }
