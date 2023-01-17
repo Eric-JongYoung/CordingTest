ea7f87d7deee3d92d890596c0ef1ead3b8011fce
Eric-JongYoung <fgh1937@yahoo.com>
2023-01-18 00:45:53 +0900

    step08 - ch07, 8, 9
    - 조합의 경우수
    - 수열 추측하기
    - 조합구하기

---
+++ src/inflearn/Step08_DFS_BFS_활용/ch07_조합의경우수_메모이제이션/Main.java
@@ -0,0 +1,24 @@
+package inflearn.Step08_DFS_BFS_활용.ch07_조합의경우수_메모이제이션;
+
+import java.util.Scanner;
+
+public class Main {
+    int[][] dy = new int[35][35];
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int r = kb.nextInt();
+        System.out.println(T.DFS(n, r));
+    }
+
+    public int DFS(int n, int r) {
+        if(dy[n][r]>0) return dy[n][r]; // 이미 저장된 값이면 바로 해당 값을 내보내라.
+        if (n == r || r ==0 ) return 1;
+        else return dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r);
+    }
+}
+
+/**
+ * 한번 구해진 값은 2차원 배열에 기록
+ * */
---
+++ src/inflearn/Step08_DFS_BFS_활용/ch09_조합구하기/Main.java
@@ -0,0 +1,44 @@
+package inflearn.Step08_DFS_BFS_활용.ch09_조합구하기;
+
+import java.util.Scanner;
+
+public class Main {
+    static int[] combi;
+    static int n, m;
+
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        m = kb.nextInt();
+        combi = new int[m];
+        T.DFS(0, 1);
+    }
+
+    public void DFS(int L, int s) {
+        if(L==m){
+            for(int x : combi) System.out.print(x + " ");
+            System.out.println();
+        }else {
+            for (int i = s; i <= n; i++) {
+                combi[L] = i;
+                DFS(L+1, i+1);
+            }
+        }
+    }
+}
+
+/**
+ 1부터N까지번호가적힌구슬이있습니다.이중 M개를뽑는방법의수를출력하는프로그 램을 작성하세요.
+ ▣ 입력설명
+ 첫 번째 줄에 자연수 N(3<=N<=10)과 M(2<=M<=N) 이 주어집니다.
+ ▣ 출력설명
+ 첫 번째 줄에 결과를 출력합니다.
+ 출력순서는 사전순으로 오름차순으로 출력합니다.
+ ▣ 입력예제 1 42
+ ▣ 출력예제 1 12
+ 13
+ 14
+ 23 24 34
+ * */
